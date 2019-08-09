fe50ce78b132f09e2764923dc9b4f0b198261d1f
Sergio Rodriguez Orellana <sergio@lucidchart.com>
2019-08-09 16:21:49 -0600

    publish tagged builds only
    publish version is based on tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,17 +8,22 @@ bazel build twirl-compiler:twirl-compiler_deploy.jar
 # Generate pom.xml
 bazel build twirl-compiler:pom
 
+# The version to publish under is specified by the TRAVIS_TAG environment variable, which is loaded by a workspace rule
+artifactId="twirl-compiler-cli"
+version=$(cat $(bazel info output_base)/external/env_vars/env_vars.bzl | grep "TRAVIS_TAG" | awk -F '"' '{print $2}')
+if [ -z "$version" ]; then
+  echo "version is not defined. Aborting publish to Maven."
+  exit 1
+fi
+
 deploy_jar="bazel-out/k8-fastbuild/bin/twirl-compiler/twirl-compiler_deploy.jar"
 pom_file="bazel-out/k8-fastbuild/bin/twirl-compiler/pom.xml"
-
-artifactId="twirl-compiler-cli"
-version=$(cat twirl-compiler/BUILD.bazel | egrep "^version" | awk -F '"' '{print $2}')
-source_jar="temp/$artifactId-$version-sources.jar"
-javadoc_jar="temp/$artifactId-$version-javadoc.jar"
 
 # Maven requires a source jar and a javadoc jar to be included, but this is a Scala project
 mkdir -p temp
 echo "empty jar" > temp/README
+source_jar="temp/$artifactId-$version-sources.jar"
+javadoc_jar="temp/$artifactId-$version-javadoc.jar"
 jar -cf "$source_jar" temp/README
 jar -cf "$javadoc_jar" temp/README
 
@@ -31,7 +36,7 @@ else
 fi
 
 # Deploy to maven
-echo "Deploying $deploy_jar to $url"
+echo "Deploying $artifactId-$version to $url"
 mvn -e --fail-at-end gpg:sign-and-deploy-file \
 	-Dfile="$deploy_jar" \
 	-DpomFile="$pom_file" \
@@ -41,4 +46,5 @@ mvn -e --fail-at-end gpg:sign-and-deploy-file \
   -Dsources="$source_jar" \
 	--settings=".mvn_settings.travis.xml"
 
+rm -r temp
 echo "Deployment complete."
